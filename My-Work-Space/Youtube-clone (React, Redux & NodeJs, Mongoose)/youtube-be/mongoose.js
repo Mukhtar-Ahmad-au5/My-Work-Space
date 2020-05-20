c61001457a5d5326658a6556cf2dae64a5a04817
Mukhtar's MacBook Pro <macbookpro@MacBook-Pro.local>
2020-05-20 13:00:55 +0530

    deleted-older-files

--- My-Work-Space/Youtube-clone (React, Redux & NodeJs, Mongoose)/youtube-be/mongoose.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const config = require("config");
-
-const db = config.get('mongoURI')
-
-mongoose.connect(db , {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true}).then(res => {
-    console.log("succesfully connected mongoose")
-})
-.catch(err => {console.log("error didn't connect to mongoose", err)})
--- My-Work-Space/Youtube-clone (React, Redux & NodeJs, Mongoose)/youtube-be/routes/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const router = express.Router()
-// const mongoose = require("../mongoose")
-
-router.use("/users", require("./users"))
-router.use("/videos", require("./videos"))
-
-module.exports = router;
